e2479e477a7b3255aa128689d0d4fa61e815e6d4
Rishabh2457 <rishabh.kushwah_cs.aiml21@gla.ac.in>
2024-08-26 23:30:39 +0600

    Timezone_manager

--- workstatus_agent/main.py
+++ workstatus_agent/main.py
@@ -1,16 +1,23 @@
 import threading
 from screenshot_manager import ScreenshotManager
 from config_manager import ConfigManager
+from timezone_manager import TimezoneManager
 
 def main():
     try:
         config_manager = ConfigManager()
         config_manager.load_config()
         screenshot_manager = ScreenshotManager(config_manager.config)
+        # Initialize TimezoneManager
+        timezone_manager = TimezoneManager()
 
         # Start screenshot thread
         threading.Thread(target=screenshot_manager.start_screenshot_loop).start()
         print("ScreenshotManager thread started.")
+
+        # Start timezone detection thread
+        threading.Thread(target=timezone_manager.detect_timezone_change).start()
+        print("TimezoneManager thread started.")
         
     except Exception as e:
         print(f"An error occurred: {str(e)}")

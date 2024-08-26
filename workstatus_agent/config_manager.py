3ededa028ad884c8d6c54b3c9041d4b5f701a825
PankajAjmera1 <coderajmera@gmail.com>
2024-08-26 15:30:26 +0530

    Config manager

---
+++ workstatus_agent/config_manager.py
@@ -0,0 +1,11 @@
+import json
+
+class ConfigManager:
+    def __init__(self, config_path='config.json'):
+        self.config_path = config_path
+        self.load_config()
+
+    def load_config(self):
+        with open(self.config_path, 'r') as file:
+            self.config = json.load(file)
+

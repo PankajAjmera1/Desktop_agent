03d03b6151192896f66223f12a5af64ed98e0345
PankajAjmera1 <coderajmera@gmail.com>
2024-08-27 15:16:51 +0530

    added environment security using dotenv

--- workstatus_agent/config_manager.py
+++ workstatus_agent/config_manager.py
@@ -1,4 +1,6 @@
 import json
+from dotenv import load_dotenv
+import os
 
 class ConfigManager:
     def __init__(self, config_path='config.json'):
@@ -8,4 +10,11 @@ class ConfigManager:
     def load_config(self):
         with open(self.config_path, 'r') as file:
             self.config = json.load(file)
+
+        self.config['aws_access_key'] = os.getenv('AWS_ACCESS_KEY')
+        self.config['aws_secret_key'] = os.getenv('AWS_SECRET_KEY')
+
+
+    def get_config(self):
+        return self.config
 
